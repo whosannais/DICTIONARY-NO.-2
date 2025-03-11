ddd3749be7e7534a35707d735b0a4e2ef8619997
whosannais <annalynlulu02@gmail.com>
2025-03-11 19:31:11 +0800

    Add files via upload

---
+++ ACTIVITY #2.py
@@ -0,0 +1,9 @@
+def check_number(num):
+    if num % 2 == 0:
+        print(f"{num} is an Even number.")
+    else:
+        print(f"{num} is an Odd number.")
+
+# Example test cases
+check_number(4)
+check_number(7)
